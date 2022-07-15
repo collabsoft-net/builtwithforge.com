0fc7b60f84aa906e158af6e331a20ccddb71995b
Remie Bolte <remie@collabsoft.net>
2022-07-15 22:04:35 +0200

    feat: add links to app & partner

--- frontend/src/modules/Index/index.tsx
+++ frontend/src/modules/Index/index.tsx
@@ -87,12 +87,12 @@ const createRows = (apps: Array<AppDTO>) => {
 
     row.cells.push({
       key: app.name,
-      content: app.name
+      content: <a href={`https://marketplace.atlassian.com/apps/${app.id}?tab=overview&hosting=cloud`} target="_blank">{app.name}</a>
     });
 
     row.cells.push({
       key: app.partner.name,
-      content: app.partner.name
+      content: <a href={`https://marketplace.atlassian.com/vendors/${app.partner.id}`} target="_blank">{app.partner.name}</a>
     });
 
     row.cells.push({
@@ -141,52 +141,59 @@ export const Index = () => {
 
   return (
     <Page padding='40px 0'>
-      <Grid>
+      <Grid fluid padding="0 20px">
         <Row>
-          <Grid alignItems='center'>
+          <Grid>
             <Row>
-              <Header weight='h900'>State of Forge</Header>
+              <Grid alignItems='center'>
+                <Row>
+                  <Header weight='h900'>State of Forge</Header>
+                </Row>
+              </Grid>
+            </Row>
+            <Row margin="16px 0 0">
+              <Paragraph>
+                On July 8th, 2022, Atlassian <a href="https://blog.developer.atlassian.com/its-time-to-celebrate-forge-turns-one/" target="_blank">celebrated the first anniversary of Forge</a>, 
+                their new platform for creating Cloud apps for the Atlassian product suite. The article highlights a lot of the milestones that the platform has reached during the early stages, 
+                including the number of downloads of the CLI and the amount of apps created. 
+              </Paragraph>
+              <Paragraph>
+                Although the target audience for Forge is the broader Atlassian Ecosystem, which includes in-house developers and hobbyists, there is a select group of people that develop apps for a living. 
+                The (paid) apps that they create are published to the Atlassian Marketplace. This group is called the Atlassian Marketplace Partners, and they take a special interest in Forge as Atlassian
+                has indicated that this will be the preferred platform to create Cloud apps.
+              </Paragraph>
+              <Paragraph>
+                That is why this group is particulairy interested in the progress of Forge, and more specifically, have detailed insights into the current state of forge with regard to commercial exploitation
+                of Forge apps. Unfortunately, Atlassian does not provide much information about Forge apps. For instance, it is not possible to filter Forge apps on the Atlassian Marketplace itself.
+              </Paragraph>
+              <Paragraph>
+                Which is why we spent some time creating a more complete overview of the State of Forge, by providing a daily updated list of Forge apps as listed on the Atlassian Marketplace. You can browse
+                the list and filter it. The purpose of this list is to provide more insights to power your business decisions.
+              </Paragraph>
+              <Paragraph>
+                <Header weight='h300'>Ok, enough talk, show me the list of Forge apps!</Header>
+              </Paragraph>
             </Row>
           </Grid>
         </Row>
-        <Row margin="16px 0 0">
-          <Paragraph>
-            On July 8th, 2022, Atlassian <a href="https://blog.developer.atlassian.com/its-time-to-celebrate-forge-turns-one/" target="_blank">celebrated the first anniversary of Forge</a>, 
-            their new platform for creating Cloud apps for the Atlassian product suite. The article highlights a lot of the milestones that the platform has reached during the early stages, 
-            including the number of downloads of the CLI and the amount of apps created. 
-          </Paragraph>
-          <Paragraph>
-            Although the target audience for Forge is the broader Atlassian Ecosystem, which includes in-house developers and hobbyists, there is a select group of people that develop apps for a living. 
-            The (paid) apps that they create are published to the Atlassian Marketplace. This group is called the Atlassian Marketplace Partners, and they take a special interest in Forge as Atlassian
-            has indicated that this will be the preferred platform to create Cloud apps.
-          </Paragraph>
-          <Paragraph>
-            That is why this group is particulairy interested in the progress of Forge, and more specifically, have detailed insights into the current state of forge with regard to commercial exploitation
-            of Forge apps. Unfortunately, Atlassian does not provide much information about Forge apps. For instance, it is not possible to filter Forge apps on the Atlassian Marketplace itself.
-          </Paragraph>
-          <Paragraph>
-            Which is why we spent some time creating a more complete overview of the State of Forge, by providing a daily updated list of Forge apps as listed on the Atlassian Marketplace. You can browse
-            the list and filter it. The purpose of this list is to provide more insights to power your business decisions.
-          </Paragraph>
-          <Paragraph>
-            <Header weight='h300'>Ok, enough talk, show me the list of Forge apps!</Header>
-          </Paragraph>
-        </Row>
-        <Row margin="16px 0 0">
-          { !isLoading && (
-            <Grid fluid vertical>
-              <Column>{apps.length} Forge apps listed</Column>
-            </Grid>
-          )}
-        </Row>
-      </Grid>
-      <Grid fluid padding="16px 20px">
         <Row>
-          <DynamicTable
-            head={ createHead() }
-            rows={ createRows(apps) }
-            emptyView={ <span>There are no Forge apps available</span> }
-            isLoading={ isLoading } />
+          <Grid fluid padding="16px 20px">
+            <Row>
+              <Grid fluid vertical>
+                <Column stretched></Column>
+                { !isLoading && (
+                  <Column>{apps.length} Forge apps listed</Column>
+                )}
+              </Grid>
+            </Row>
+            <Row margin="12px 0">
+              <DynamicTable
+                head={ createHead() }
+                rows={ createRows(apps) }
+                emptyView={ <span>There are no Forge apps available</span> }
+                isLoading={ isLoading } />
+            </Row>
+          </Grid>
         </Row>
       </Grid>
     </Page>
